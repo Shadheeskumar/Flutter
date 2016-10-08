658c2cd31ef480bfc365e4d13de508f5908f4850
SHADHEESKUMAR <SHADHEESKUMAR@gmail.com>
2016-10-09 00:34:09 +0800

    created pages controller, explore, home, index, profile pages, changed url

---
+++ flutter/app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+# this pages controller contains all the code for any page inside pages
+class PagesController < ApplicationController
+  def index
+  end
+
+  def home
+  end
+
+  def profile
+  end
+
+  def explore
+  end
+  
+end
